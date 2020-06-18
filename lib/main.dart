ee1c93dedf6b60e98801d5a98c40c6d591bf5005
Troy Cardozo <t.cardozo@aaigl.com.au>
2020-06-19 00:58:44 +1000

    added food cards & floating button

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,9 @@ class MyApp extends StatelessWidget {
           headline6: TextStyle(
             fontWeight: FontWeight.bold,
           ),
+          bodyText1: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
           button: TextStyle(fontWeight: FontWeight.bold),
         ),
         visualDensity: VisualDensity.adaptivePlatformDensity,
@@ -36,6 +39,21 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: Scaffold(
+        floatingActionButton: Container(
+          padding: EdgeInsets.all(5),
+          height: 50,
+          width: 50,
+          decoration: BoxDecoration(
+              shape: BoxShape.circle, color: kPrimaryColor.withOpacity(.26)),
+          child: Container(
+            padding: EdgeInsets.all(12),
+            decoration:
+                BoxDecoration(shape: BoxShape.circle, color: kPrimaryColor),
+            child: SvgPicture.asset(
+              "assets/icons/plus.svg",
+            ),
+          ),
+        ),
         resizeToAvoidBottomInset: false,
         body: SafeArea(
           child: SingleChildScrollView(
@@ -46,17 +64,155 @@ class HomeScreen extends StatelessWidget {
                 pageTitle(context),
                 categoryScrollList(),
                 SearchFoodBox(),
-                Container(
-                  height: 300,
-                  width: 270,
-                  child: Stack(
-                    children: <Widget>[],
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: <Widget>[
+                      FoodCard(
+                        title: "Vegan salad bowl",
+                        price: 20,
+                        image: "image_1.png",
+                      ),
+                      FoodCard(
+                        title: "Vegan salad bowl",
+                        price: 40,
+                        image: "image_2.png",
+                      ),
+                      SizedBox(
+                        width: 20,
+                      )
+                    ],
                   ),
                 )
               ],
             ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class FoodCard extends StatelessWidget {
+  final String title;
+  final String ingredient;
+  final String image;
+  final int price;
+  final String description;
+  final String calories;
+
+  const FoodCard({
+    Key key,
+    this.title,
+    this.ingredient,
+    this.image,
+    this.price,
+    this.description,
+    this.calories,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(left: 10),
+      height: 250,
+      width: 185,
+      child: Stack(
+        children: <Widget>[
+          //Big light background
+          Positioned(
+            right: 0,
+            bottom: 0,
+            child: Container(
+              height: 240,
+              width: 175,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(34),
+                  color: kPrimaryColor.withOpacity(.06)),
+            ),
+          ),
+
+          //Rounded Background
+          Positioned(
+            left: 5,
+            top: 5,
+            child: Container(
+              height: 120.5,
+              width: 120.5,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: kPrimaryColor.withOpacity(.15),
+              ),
+            ),
+          ),
+
+          //Food image
+          Positioned(
+            top: 0,
+            left: -30,
+            child: Container(
+              height: 120,
+              width: 180,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/images/" + image)),
+              ),
+            ),
+          ),
+
+          //price
+          Positioned(
+            right: 20,
+            top: 50,
+            child: Text("\$" + price.toString(),
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyText1
+                    .copyWith(color: kPrimaryColor)),
+          ),
+          Positioned(
+            top: 125,
+            left: 20,
+            child: Container(
+              width: 150,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    title,
+                    style: Theme.of(context).textTheme.bodyText1,
+                  ),
+                  Text(
+                    "With red tomato",
+                    style: Theme.of(context)
+                        .textTheme
+                        .caption
+                        .copyWith(color: kTextColor.withOpacity(.4)),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    "Conrary to popular belief, Lorem ipsum is not simply random text. It has",
+                    maxLines: 4,
+                    style: Theme.of(context).textTheme.caption.copyWith(
+                        color: kTextColor.withOpacity(.7), fontSize: 10),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    "420Kcal",
+                    style: Theme.of(context).textTheme.caption.copyWith(
+                        color: kTextColor.withOpacity(.7),
+                        fontSize: 11,
+                        fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
@@ -71,7 +227,7 @@ class SearchFoodBox extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       alignment: Alignment.centerLeft,
-      margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
       padding: EdgeInsets.symmetric(horizontal: 10),
       width: double.infinity,
       height: 40,
